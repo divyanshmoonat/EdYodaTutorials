7559a19914392f3ee414c0e1635ffe1f11a91fa6
Divyansh Moonat <DivyanshM@edynamic.net>
2022-06-07 19:31:11 +0530

    6 June

---
+++ JS/6-June-2022/script.js
@@ -0,0 +1,95 @@
+var greeting1 = "Hi";
+var userName = "John";
+var greeting2 = "Welcome to EdYoda";
+
+// console.log(greeting1 + " " + userName + " " + greeting2);
+
+// console.log(`Hi, ${userName} Welcome to EdYoda`);
+
+// console.log(`${greeting1} ${userName} ${greeting2}`);
+
+// var upperCaseString = greeting2.toUpperCase();
+
+// var index = upperCaseString.indexOf('TO');
+
+// console.log(greeting2.indexOf('EdYoda').substring(1,2));
+
+// var person = {
+//     fName: 'John',
+//     lName: 'Doe'
+// };
+
+// console.log(person.fName);
+
+// console.log(console);
+
+// console.log(1+1);
+// console.log('1'+'1');
+// console.log('1'+1); // Implicit type coersion/conversion
+
+// console.log(true + true + true + true + false + false + true);
+
+// console.log(5 + true);
+
+// console.log(true + 3 + 'true');
+
+// console.log("John" + 3 + "ABC");
+
+// console.log('10'+'1');
+
+// console.log('10'-'1');
+
+
+// console.log('John' - 1);
+
+// ||
+
+// true || true => true
+
+// false || true => true
+
+// false || false => false
+
+// console.log(typeof(null));
+// console.log(typeof(undefined));
+// null==undefined
+// null===undefined
+
+
+
+// var num1 = "10";
+
+// var num1 = prompt("Enter a number");
+
+// num1 = Number(num1);
+
+// if(num1 === NaN) {
+//     console.log("You have entered an invalid number");
+// } else {
+//     if(num1 < 10) {
+//         console.log("True block");
+//     } else {
+//         console.log("False block");
+//     }
+// }
+
+
+// console.log(num1 + 5);
+
+// var num1;
+// var num2;
+// var num3;
+
+
+// var num1,num2,num3;
+
+var expression = !!"John" == !!"Doe"
+var expresion = !!"false" == !!"true";
+
+expression = !!true == !!true;
+
+expression = !false == !false;
+
+expression = true == true;
+
+expression = true;
